e4acd7d3985300f818a358993e3a34c9aefb7af3
juseonyoung <ssyy0622@gmail.com>
2020-09-16 16:22:04 +0900

    0916과제

--- hello_node_3/routes/index.js
+++ hello_node_3/routes/index.js
@@ -5,7 +5,16 @@ const moment = require("moment");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
-    title: "주선영님 반갑습니다",
+    title: "반갑습니다",
+    date: moment(new Date()).format("YYYY-MM-DD"),
+    time: moment(new Date()).format("HH:mm:ss"),
+  });
+});
+
+router.post("/", function (req, res, next) {
+  name = req.body.name;
+  res.render("index", {
+    title: name + "님 반갑습니다",
     date: moment(new Date()).format("YYYY-MM-DD"),
     time: moment(new Date()).format("HH:mm:ss"),
   });
